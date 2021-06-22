5263f2b5baa4f1f357f2b78725231f477f306709
ElectroPerf <kunmun.devroms@gmail.com>
2021-06-23 01:13:28 +0530

    scripts : Walkerz-IfY!

---
+++ whyred.sh
@@ -0,0 +1,256 @@
+#! /bin/bash
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+#Kernel building script
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
+# The name of the device for which the kernel is built
+MODEL="Xiaomi Redmi Note 5 Pro / AI"
+# The codename of the device
+DEVICE="whyred"
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=whyred_defconfig
+# Show manufacturer info
+MANUFACTURERINFO="Xiaomi Inc."
+# Specify compiler.
+# 'clang' or 'clangxgcc' or 'gcc'
+COMPILER=gcc
+	if [ $COMPILER = "clang" ] || [ $COMPILER = "clangxgcc" ]
+	then
+		# install few necessary packages
+		sudo apt-get -y install gcc llvm lld g++-multilib clang
+	fi
+# Kernel is LTO
+LTO=0
+# Clean source prior building. 1 is NO(default) | 0 is YES
+INCREMENTAL=1
+# Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
+DEF_REG=0
+# Build dtbo.img (select this only if your source has support to building dtbo.img)
+# 1 is YES | 0 is NO(default)
+BUILD_DTBO=0
+# Sign the zipfile
+# 1 is YES | 0 is NO
+SIGN=0
+	if [ $SIGN = 1 ]
+	then
+		#Check for java
+		if command -v java > /dev/null 2>&1; then
+			SIGN=1
+		else
+			SIGN=0
+		fi
+	fi
+# Debug purpose. Send logs on every successfull builds
+# 1 is YES | 0 is NO(default)
+LOG_DEBUG=0
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
+# Check if we are using a dedicated CI ( Continuous Integration ), and
+# set KBUILD_BUILD_VERSION and KBUILD_BUILD_HOST and CI_BRANCH
+#Check Kernel Version
+LINUXVER=$(make kernelversion)
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
+# Set Date
+DATE=$(TZ=Asia/Kolkata date +"%Y-%m-%d")
+#Now Its time for other stuffs like cloning, exporting, etc
+ clone() {
+	echo " "
+	if [ $COMPILER = "clang" ]
+	then
+		msg "|| Cloning toolchain ||"
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+	elif [ $COMPILER = "gcc" ]
+	then
+		msg "|| Cloning GCC ||"
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1
+                git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		msg "|| Cloning toolchain ||"
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
+		msg "|| Cloning GCC ||"
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1
+		git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+	fi
+	# Toolchain Directory defaults to clang-llvm
+		TC_DIR=$KERNEL_DIR/clang
+	# GCC Directory
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
+	msg "|| Cloning Anykernel ||"
+        git clone https://github.com/ElectroPerf/AnyKernel3.git -b ElectroPerf-P-Wifi
+	if [ $BUILD_DTBO = 1 ]
+	then
+		msg "|| Cloning libufdt ||"
+		git clone https://android.googlesource.com/platform/system/libufdt "$KERNEL_DIR"/scripts/ufdt/libufdt
+	fi
+}
+##------------------------------------------------------##
+exports() {
+	export KBUILD_BUILD_USER="P.M.SALMAN KHAN"
+	export ARCH=arm64
+	export SUBARCH=arm64
+	if [ $COMPILER = "clang" ]
+	then
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin/:$PATH
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:/usr/bin:$PATH
+	elif [ $COMPILER = "gcc" ]
+	then
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+	fi
+	if [ $LTO = "1" ];then
+		export LD=ld.lld
+        export LD_LIBRARY_PATH=$TC_DIR/lib
+	fi
+	export PATH KBUILD_COMPILER_STRING
+	PROCS=$(nproc --all)
+	export PROCS
+}
+##----------------------------------------------------------##
+# Function to replace defconfig versioning
+setversioning() {
+    # For staging branch
+    KERNELNAME="WALKERZ~ifY-x-ULTRA-x-STRIXEAS"
+    # Export our new localversion and zipnames
+    export KERNELNAME
+    export ZIPNAME="$KERNELNAME.zip"
+}
+##--------------------------------------------------------------##
+build_kernel() {
+	if [ $INCREMENTAL = 0 ]
+	then
+		msg "|| Cleaning Sources ||"
+		make clean && make mrproper && rm -rf out
+	fi
+	msg "|| Started Compilation ||"
+	make O=out $DEFCONFIG
+	if [ $DEF_REG = 1 ]
+	then
+		cp .config arch/arm64/configs/$DEFCONFIG
+		git add arch/arm64/configs/$DEFCONFIG
+		git commit -m "$DEFCONFIG: Regenerate
+						This is an auto-generated commit"
+	fi
+	BUILD_START=$(date +"%s")
+	if [ $COMPILER = "clang" ]
+	then
+		make -j"$PROCS" O=out \
+				CROSS_COMPILE=aarch64-linux-gnu- \
+				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+				CC=clang \
+				AR=llvm-ar \
+				OBJDUMP=llvm-objdump \
+				STRIP=llvm-strip
+	elif [ $COMPILER = "gcc" ]
+	then
+		make -j"$PROCS" O=out \
+				CROSS_COMPILE_ARM32=arm-eabi- \
+				CROSS_COMPILE=aarch64-elf- \
+				AR=aarch64-elf-ar \
+				OBJDUMP=aarch64-elf-objdump \
+				STRIP=aarch64-elf-strip
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		make -j"$PROCS"  O=out \
+					CC=clang \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+					AR=llvm-ar \
+					AS=llvm-as \
+					NM=llvm-nm \
+					STRIP=llvm-strip \
+					OBJCOPY=llvm-objcopy \
+					OBJDUMP=llvm-objdump \
+					OBJSIZE=llvm-size \
+					READELF=llvm-readelf \
+					HOSTCC=clang \
+					HOSTCXX=clang++ \
+					HOSTAR=llvm-ar \
+					CLANG_TRIPLE=aarch64-linux-gnu-
+	fi
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+	if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+	then
+		msg "|| Kernel successfully compiled ||"
+	elif ! [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ]
+	then
+		echo -e "Kernel compilation failed, See buildlog to fix errors"
+	fi
+	if [ $BUILD_DTBO = 1 ]
+	then
+		msg "|| Building DTBO ||"
+		python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+			create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
+	fi
+}
+##--------------------------------------------------------------##
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+	if [ $BUILD_DTBO = 1 ]
+	then
+		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+	fi
+	cd AnyKernel3 || exit
+        cp -af anykernel-real.sh anykernel.sh
+	sed -i "s/kernel.string=.*/kernel.string=ElectroPerf-R-CAF-STABLE/g" anykernel.sh
+	sed -i "s/kernel.for=.*/kernel.for=P-WIFI/g" anykernel.sh
+	sed -i "s/kernel.compiler=.*/kernel.compiler=EVA-GCC/g" anykernel.sh
+	sed -i "s/kernel.made=.*/kernel.made=Kunmun @ElectroPerf/g" anykernel.sh
+	sed -i "s/kernel.version=.*/kernel.version=$LINUXVER/g" anykernel.sh
+	sed -i "s/message.word=.*/message.word=Appreciate your efforts for choosing ElectroPerf kernel./g" anykernel.sh
+	sed -i "s/build.date=.*/build.date=$DATE/g" anykernel.sh
+	zip -r9 "$ZIPNAME" * -x .git README.md anykernel-real.sh .gitignore *.zip
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME"
+	if [ $SIGN = 1 ]
+	then
+		## Sign the zip before sending it to telegram
+		if [ "$PTTG" = 1 ]
+ 		then
+ 			msg "|| Signing Zip ||"
+			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>"
+ 		fi
+		java -jar zipsigner-3.0.jar "$KERNELNAME".zip "$KERNELNAME"-signed.zip
+		ZIP_FINAL="$ZIP_FINAL-signed"
+	fi
+	cd ..
+}
+setversioning
+clone
+exports
+build_kernel
+gen_zip
+##------------------------------------------------------------------##
